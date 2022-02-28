0e384deb8b5439237294ed417fc5c44611984d22
Sachin Thirumala <sachin.thirumala@gmail.com>
2022-02-28 19:16:04 +0530

    Initial commit

---
+++ src/main/java/com/stdatalabs/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,30 @@
+package com.stdatalabs.betterreadsdataloader;
+
+import connection.DatastaxAstraProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DatastaxAstraProperties.class)
+public class BetterReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+	/**
+	 * This is necessary to have the Spring Boot app use the Astra secure bundle
+	 * to connect to the database
+	 */
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DatastaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+}
